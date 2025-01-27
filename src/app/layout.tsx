b02c524fe9a3dbdf0ce6e8ecba139900086828d4
Michael O. Ajayi <ajayimichael@hotmail.com>
2025-01-27 21:40:05 +0100

    Some visual modifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import bgImage from "../../public/images/bg-image.png";
 
 import { cormorant, satisfy } from "./fonts";
 
+import Image from "next/image";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,7 +19,15 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${cormorant.variable} ${satisfy.variable} antialiased`}>
-        {children}
+        <div className='w-screen h-screen relative overflow-hidden'>
+          <Image
+            src={bgImage}
+            alt='bg image'
+            className='absolute inset-0 object-cover w-full h-full scale-125 blur-[1px]'
+          />
+          <div className='absolute inset-0 bg-black bg-opacity-[50%]'></div>
+          {children}
+        </div>
       </body>
     </html>
   );
